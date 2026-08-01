f931b4e6726715f13f774d2e3d2d6d848f9f42d5
Kimnkitae <kimnkitae@gmail.com>
2026-08-01 17:08:44 +0300

    fix: Header.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import Header from './Layout/Header'
-import Footer from './Layout/Footer'
+
+import { BrowserRouter } from 'react-router-dom'
+import './App.css'
 
 function App() {
  
 
   return (
     <>
+    <BrowserRouter>
       <Header />
 
-      <Footer />
+
+    </BrowserRouter>
     </>
   )
 }
--- src/Layout/Header.tsx
+++ src/Layout/Header.tsx
@@ -1,9 +1,22 @@
-import 'Header.css'
+import { Link } from 'react-router-dom'
+import './Header.css'
 
 function Header() {
   return (
     <>
-    
+      <header className="header">
+        <div className="header-logo">
+          <Link to="/feed">
+            <img src="src/assets/icons/logo.svg" alt="Logo" />
+          </Link>
+        </div>
+        <div className="header-nav">
+          <input type="text" placeholder="Search" />
+        </div>
+        <div className="header-profile">
+          <img></img>
+        </div>
+      </header>
     </>
 
   )
